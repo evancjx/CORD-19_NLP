a73bbce78088de1632029e577554064107c4d8c7
Evan <evancjx@gmail.com>
2020-06-04 16:18:22 +0800

    Script to conduct TF-IDF from sklearn functions

--- src/tf_idf.py
+++ src/tf_idf.py
@@ -64,3 +64,61 @@ def corpus_tf_idf(corpus):
         doc_tf_idf(doc_tf, term_doc_freq, len(corpus_doc_tf))
         for doc_tf in corpus_doc_tf
     ], term_doc_freq
+
+from .text_preprocessing import STOP_WORDS
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+import numpy as np
+class sklearn_TFIDF(object):
+    feature_names = None
+
+    def __init__(
+        self, 
+        count_vectorizer=CountVectorizer(
+            max_df=0.8, stop_words=STOP_WORDS, max_features=10000, ngram_range=(1,1)
+        ), 
+        tfidf_transformer=TfidfTransformer(
+            smooth_idf=True,use_idf=True
+        )
+    ):
+        self.count_vectorizer = count_vectorizer
+        self.tfidf_transformer = tfidf_transformer
+    
+    def tfidf_corpus(self, corpus):
+        self.corpus = corpus
+
+        self.tfidf_transformer.fit(
+            self.count_vectorizer.fit_transform(
+                corpus
+            )
+        )
+    
+        self.feature_names = self.count_vectorizer.get_feature_names()
+
+    def tfidf_text(self, text):
+        if self.feature_names is None:
+            print('Conduct tfidf on corpus')
+            return None
+        if not isinstance(text, list):
+            text = [text]
+
+        return self.tfidf_transformer.transform(
+            self.count_vectorizer.transform(
+                text
+            )
+        )
+
+    # sort coordinate matrix based by score then word index
+    def sort_coo(self, coo_matrix, descending=True):
+        return {
+            k: v
+            for k, v in sorted(
+                zip(coo_matrix.col, coo_matrix.data), 
+                key=lambda x: (x[1], x[0]), 
+                reverse=descending
+            )
+        }
+
+    def get_text_keywords(self, text, n_keywords=10):
+        return np.array(self.feature_names)[
+            list(self.sort_coo(self.tfidf_text(text).tocoo()).keys())[:n_keywords]
+        ]
